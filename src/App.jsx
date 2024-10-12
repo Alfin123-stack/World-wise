cbe220f3aa0f2d3f7fd9e118ffb30b448e784af1
Alfin123-stack <alfinkarin789@gmail.com>
2024-10-12 19:46:27 +0700

    add alot of components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
